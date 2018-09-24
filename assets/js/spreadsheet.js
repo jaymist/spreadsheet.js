5155130eca14d14a10c41d29939e718a8258bc3b
Jayesh Mistry <jmistry@perforce.com>
2018-09-24 19:37:56 +1200

    Create table heading row.
    Use a simple for loop to count from 0 to 100, converting the index value
    to its corresponding ascii character.  Those indexes over 26 will convert
    to double character headings; for example, AA, AB, AC, ...

--- assets/js/spreadsheet.js
+++ assets/js/spreadsheet.js
@@ -1,9 +1,56 @@
+//-----------------------------------------------------------------------------
+// GLOBAL VARIABLES
+//-----------------------------------------------------------------------------
+var gCharCodeA  = "A".charCodeAt (); // Comes out as '65'
+var gCharCount  = 26;
+
+function PositionToStr (pos) {
+    var remainder   = pos % gCharCount;
+    var quotient    = Math.floor (pos / gCharCount );
+    var str         = String.fromCharCode (remainder + gCharCodeA);
+
+    if (quotient < 1)
+        return str;
+
+            // Get whole number quotient
+    str = String.fromCharCode (quotient + gCharCodeA - 1) + str;
+    return str;
+};
+
 // Create a new grid within the window.
 function CreateTable (parent) {
-    var table = $("<table/>");
+    var table = $("<table>");
 
     table.appendTo (parent);
+
+    InsertTableHeading (table);
 };
+
+// Create top table row and add to parent.
+function InsertTableHeading (parent) {
+    var row = $("<tr>");
+    row.addClass ("heading-row");
+
+    var heading      = $("<th>");
+    heading.addClass ("heading");
+    heading.appendTo (row);
+
+            // Create the initial header row.
+    for (x = 0; x < 100; ++x)
+    {
+        var heading      = $("<th>");
+        heading.addClass ("heading");
+    
+                    // Convert for loop position to ASCII characters from A..ZZ
+        var headerText   = PositionToStr (x);
+        console.log      ("Character string: %s", headerText);
+
+        heading.text     (headerText);
+        heading.appendTo (row);
+    }
+
+    row.appendTo (parent);
+}
 
 //-----------------------------------------------------------------------------
 // MAIN ENTRY POINT
